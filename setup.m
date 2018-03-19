db748703fe161a2c4be335928840f77667e6a801
Tom Bruijnen <T.bruijnen@umcutrecht.nl>
2018-03-19 13:58:28 +0100

    Improved toolbox install checks

--- setup.m
+++ setup.m
@@ -1,5 +1,5 @@
 %% Setup paths appropriately
-%addpath(genpath('../Main_Recon_Library'))
-addpath(genpath('..\Main_Recon_Library\'))
-%addpath(fullfile('../Modular_Recon_Library/standalone/bart-0.4.02/','matlab'));
-%setenv('TOOLBOX_PATH','../Modular_Recon_Library/standalone/bart-0.4.02');
+addpath(genpath('../Main_Recon_Library'))
+%addpath(genpath('..\Main_Recon_Library\'))
+addpath(fullfile('../Main_Recon_Library/standalone/bart-0.4.02/','matlab'));
+setenv('TOOLBOX_PATH','../Main_Recon_Library/standalone/bart-0.4.02');
